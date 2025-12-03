aeca5694aa9d471c9c1cd7236b36e568523af4b7
dariel <d.vanderzwan@student.talland.nl>
2025-12-03 09:39:23 +0100

    seeder

---
+++ PorkySMP-backend/database/seeders/TestSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $user = \App\Models\User::create([
+        'name' => 'Fedde',
+        'email' => 'fedde@example.com',
+        'password' => bcrypt('secret')
+        ]);
+
+        $team = \App\Models\Team::create([
+        'team_name' => 'Nederland',
+        'leader_id' => $user->id
+        ]);
+
+        $user->teams()->attach($team->id);
+
+        \App\Models\PlayerInfo::create([
+        'user_id' => $user->id,
+        'skin_url' => 'url_naar_skin',
+        'first_login' => now()
+        ]);
+    }
+}
